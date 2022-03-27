612ee076e501431861509211d78c8220d93f3cb4
chb <3277981179@qq.com>
2022-03-27 13:16:45 +0800

    2022-3-27

---
+++ JS_13_jQuery的基本使用/购物车案例/js/index.js
@@ -0,0 +1,117 @@
+$(function () {
+	// 封装一个数组求和函数
+	function NumList(list) {
+		var sun = 0;
+		for (var i = 1; i < list.length; i++) {
+			var sun = parseFloat(sun) + parseFloat(list[i]);
+		}
+		return sun;
+	}
+	// 禁止用户右键和选中
+	// contextmenu 主要控制应该何时显示上下文菜单，主要由于程序员取消默认的上下文菜单
+	document.addEventListener("contextmenu", function (e) {
+		e.preventDefault();
+	});
+	// 禁止鼠标选中(selectstart 开始选中)
+	document.addEventListener("selectstart", function (event) {
+		event.preventDefault();
+	});
+	$(".commodity")
+		.find(".remove")
+		.click(function () {
+			$(this).parent().parent(".commodity").remove();
+		});
+	$(".commodity").index();
+	var num = $(".input_val").val();
+	// console.log(num);
+	// 点击增加或减少商品模块
+	$(".add").click(function () {
+		var num = $(this).siblings("li").children(".input_val").val();
+		num++;
+		$(this).siblings().children(".input_val").val(num);
+		$(".num").html(num);
+		// 将结算好的价格保留两位小数
+		var price = (
+			num * $(this).parents(".amount").siblings(".price").text().substr(1)
+		).toFixed(2);
+		$(this)
+			.parents(".amount")
+			.siblings(".sun")
+			.html("￥" + price);
+		$(".money_sun").html("￥" + price);
+		// console.log()
+		// 将所有购物车上的价格求和 并转化为数组
+		var quantity = $(".commodity").find(".sun").text().split("￥");
+		// 将获取的数组求和
+		var priceSun = NumList(quantity).toFixed(2);
+		// 将价格的和放在结算元素里面
+		$(".money_sun").text("￥" + priceSun);
+	});
+	$(".reduce").click(function () {
+		var num = $(this).siblings("li").children(".input_val").val();
+		if (num <= 0) {
+			alert("当前数量为0 ,不能再减少");
+			num = 0;
+		} else {
+			num--;
+		}
+		// 将物品的件数赋值给 总量
+		$(".num").html(num);
+		$(this).siblings("li").siblings().children(".input_val").val(num);
+		var price = (
+			num * $(this).parents(".amount").siblings(".price").text().substr(1)
+		).toFixed(2);
+		$(this)
+			.parents(".amount")
+			.siblings(".sun")
+			.text("￥" + price);
+		// 将所有购物车上的价格求和 并转化为数组
+		var quantity = $(".commodity").find(".sun").text().split("￥");
+		// 将获取的数组求和
+		var priceSun = NumList(quantity).toFixed(2);
+		// 将价格的和放在结算元素里面
+		$(".money_sun").text("￥" + priceSun);
+	});
+	// 如果用户直接在表单里面输入商品数量 则重新计算价格
+	$(".input_val").change(function () {
+		var price =
+			$(this).val() *
+			$(this).parents(".amount").siblings(".price").text().substr(1);
+		$(this)
+			.parents(".amount")
+			.siblings(".sun")
+			.text("￥" + price);
+		// 将所有购物车上的价格求和 并转化为数组
+		var quantity = $(".commodity").find(".sun").text().split("￥");
+		// 将获取的数组求和
+		var priceSun = NumList(quantity).toFixed(2);
+		// 将价格的和放在结算元素里面
+		$(".money_sun").text("￥" + priceSun);
+	});
+	// 点击全选按钮 ，复选框全部选中 ,取消全选 复选框全部取消
+	$("#checked_all").change(function () {
+		var trans = $(this).prop("checked");
+		$("input[type = checkbox]").prop("checked", trans);
+	});
+	// 当我们单击复选框全部选中时 全选按钮应该也要选中 ：checked 查看当前有多少复选框选中
+	var check_num = $(".check").length; // 将复选框个数赋值给 check_num
+	$(".check").change(function () {
+		// 判断用户是否全部选中商品
+		if ($(".check:checked").length == check_num) {
+			$("#checked_all").prop("checked", true);
+		} else {
+			$("#checked_all").prop("checked", false);
+		}
+	});
+	// 点击删除选中的商品 选中的商品删除
+	$(".delete").click(function () {
+		if ($(".check").prop("checked") == true) {
+			// remove($('.check').parent())
+			console.log("11");
+		}
+	});
+	// 点击清空购物车 商品模块清除
+	$(".emoty").click(function () {
+		$(".commodity").remove();
+	});
+});
