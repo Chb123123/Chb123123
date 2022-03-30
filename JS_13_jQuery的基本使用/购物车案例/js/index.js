9256ab513ff8e7b1bacf2a912e566775b49c59a6
chb <3277981179@qq.com>
2022-03-30 08:31:22 +0800

    2022-3-30

--- JS_13_jQuery的基本使用/购物车案例/js/index.js
+++ JS_13_jQuery的基本使用/购物车案例/js/index.js
@@ -3,8 +3,18 @@ $(function () {
 	function NumList(list) {
 		var sun = 0;
 		for (var i = 1; i < list.length; i++) {
-			var sun = parseFloat(sun) + parseFloat(list[i]);
+			sun += parseFloat(list[i]);
 		}
+		return sun;
+	}
+	// 封装一个函数 用来结算总数量或总金额
+	function numSun(ojb, type1) {
+		// ojb 为需要遍历的对象 type1 为文本类型 例如 text val
+		// 声明一个总和
+		var sun = 0;
+		$(ojb).each(function (index, domEle) {
+			sun += $(domEle).type1();
+		});
 		return sun;
 	}
 	// 禁止用户右键和选中
@@ -16,10 +26,23 @@ $(function () {
 	document.addEventListener("selectstart", function (event) {
 		event.preventDefault();
 	});
+	// 用户点击删除 当前元素的大盒子删除 并且重新结算总和
 	$(".commodity")
 		.find(".remove")
 		.click(function () {
 			$(this).parent().parent(".commodity").remove();
+			// 将所有购物车上的价格求和 并转化为数组
+			var quantity = $(".commodity").find(".sun").text().split("￥");
+			// 将获取的数组求和
+			var priceSun = NumList(quantity).toFixed(2);
+			// 将价格的和放在结算元素里面
+			$(".money_sun").text("￥" + priceSun);
+			// 将商品的数量总和添加到 结算模块
+			var sun = 0;
+			$(".input_val").each(function (index, domEle) {
+				sun += $(domEle).val() - "";
+			});
+			$(".num").text(sun);
 		});
 	$(".commodity").index();
 	var num = $(".input_val").val();
@@ -29,7 +52,7 @@ $(function () {
 		var num = $(this).siblings("li").children(".input_val").val();
 		num++;
 		$(this).siblings().children(".input_val").val(num);
-		$(".num").html(num);
+		// $(".num").html(num);
 		// 将结算好的价格保留两位小数
 		var price = (
 			num * $(this).parents(".amount").siblings(".price").text().substr(1)
@@ -46,6 +69,13 @@ $(function () {
 		var priceSun = NumList(quantity).toFixed(2);
 		// 将价格的和放在结算元素里面
 		$(".money_sun").text("￥" + priceSun);
+		// 将商品的数量总和添加到 结算模块
+		var sun = 0;
+		$(".input_val").each(function (index, domEle) {
+			sun += $(domEle).val() - "";
+		});
+		// console.log(numSun('.input_val','val'))
+		$(".num").text(sun);
 	});
 	$(".reduce").click(function () {
 		var num = $(this).siblings("li").children(".input_val").val();
@@ -56,7 +86,7 @@ $(function () {
 			num--;
 		}
 		// 将物品的件数赋值给 总量
-		$(".num").html(num);
+		// $(".num").html(num);
 		$(this).siblings("li").siblings().children(".input_val").val(num);
 		var price = (
 			num * $(this).parents(".amount").siblings(".price").text().substr(1)
@@ -71,6 +101,12 @@ $(function () {
 		var priceSun = NumList(quantity).toFixed(2);
 		// 将价格的和放在结算元素里面
 		$(".money_sun").text("￥" + priceSun);
+		// 将商品的数量总和添加到 结算模块
+		var sun = 0;
+		$(".input_val").each(function (index, domEle) {
+			sun += $(domEle).val() - "";
+		});
+		$(".num").text(sun);
 	});
 	// 如果用户直接在表单里面输入商品数量 则重新计算价格
 	$(".input_val").change(function () {
@@ -87,11 +123,22 @@ $(function () {
 		var priceSun = NumList(quantity).toFixed(2);
 		// 将价格的和放在结算元素里面
 		$(".money_sun").text("￥" + priceSun);
+		// 将商品的数量总和添加到 结算模块
+		var sun = 0;
+		$(".input_val").each(function (index, domEle) {
+			sun += $(domEle).val() - "";
+		});
+		$(".num").text(sun);
 	});
 	// 点击全选按钮 ，复选框全部选中 ,取消全选 复选框全部取消
 	$("#checked_all").change(function () {
 		var trans = $(this).prop("checked");
 		$("input[type = checkbox]").prop("checked", trans);
+		if ($(this).prop("checked")) {
+			$(".check").parents(".commodity").css("background-color", "#fff5e7");
+		} else {
+			$(".check").parents(".commodity").css("background-color", "");
+		}
 	});
 	// 当我们单击复选框全部选中时 全选按钮应该也要选中 ：checked 查看当前有多少复选框选中
 	var check_num = $(".check").length; // 将复选框个数赋值给 check_num
@@ -105,13 +152,34 @@ $(function () {
 	});
 	// 点击删除选中的商品 选中的商品删除
 	$(".delete").click(function () {
-		if ($(".check").prop("checked") == true) {
-			// remove($('.check').parent())
-			console.log("11");
-		}
+		$(".check").each(function (index, ele) {
+			if ($(ele).prop("checked") == true) {
+				$(ele).parent().remove();
+			}
+		});
+		// 将所有购物车上的价格求和 并转化为数组
+		var quantity = $(".commodity").find(".sun").text().split("￥");
+		// 将获取的数组求和
+		var priceSun = NumList(quantity).toFixed(2);
+		// 将价格的和放在结算元素里面
+		$(".money_sun").text("￥" + priceSun);
+		// 将商品的数量总和添加到 结算模块
+		var sun = 0;
+		$(".input_val").each(function (index, domEle) {
+			sun += $(domEle).val() - "";
+		});
+		$(".num").text(sun);
 	});
 	// 点击清空购物车 商品模块清除
 	$(".emoty").click(function () {
 		$(".commodity").remove();
+	});
+	// 将选中的商品添加背景
+	$(".check").change(function () {
+		if ($(this).prop("checked")) {
+			$(this).parents(".commodity").css("background-color", "#fff5e7");
+		} else {
+			$(this).parents(".commodity").css("background-color", "");
+		}
 	});
 });
